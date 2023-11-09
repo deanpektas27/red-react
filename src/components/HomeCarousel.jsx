f13ac7e63b2e5f8f7e7c2ee6123d64478f241d7e
Dean Pektas <deanpektas12345@gmail.com>
2023-11-08 20:37:24 -0500

    Fixed carousel styling

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,31 +1,38 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CarouselImage1 from './../assets/01_Home_page/Carousel/pexels-gustavo-fring-4173169.jpg'
+import CarouselImage2 from './../assets/01_Home_page/Carousel/pexels-christina-morillo-1181605.jpg'
+import CarouselImage3 from './../assets/01_Home_page/Carousel/cytonn-photography-n95VMLxqM2I-unsplash.jpg'
 import './../styles/home-carousel.css'
 
 export default function HomeCarousel() {
     return(
+<Row>
         <Carousel>
-            <Carousel.Item>
-                <img className="d-block w-100" src="https://images.pexels.com/photos/2041627/pexels-photo-2041627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <Carousel.Item className="carousel-image bg-img-1">
+                {/* <img className="" src={CarouselImage1} alt="" /> */}
                 <Carousel.Caption>
                     <h3>LEARN FROM THE BEST IN THE INDUSTRY</h3>
                     <Button size="lg">Get Inspired</Button>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block w-100" src="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <Carousel.Item className="carousel-image bg-img-2">
+                {/* <img className="" src={CarouselImage2} alt="" /> */}
                 <Carousel.Caption>
                     <h3>BRING YOUR KNOWLEDGE</h3>
                     <Button size="lg">Volunteer</Button>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block w-100" src="https://images.pexels.com/photos/374598/pexels-photo-374598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <Carousel.Item className="carousel-image bg-img-3">
+                {/* <img className="" src={CarouselImage3} alt="" /> */}
                 <Carousel.Caption>
                     <h3>CONTRIBUTE TO STUDENTS' FUTURE</h3>
                     <Button size="lg">Donate</Button>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
+        </Row>
     )
 }

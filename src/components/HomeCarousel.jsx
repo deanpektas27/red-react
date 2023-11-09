428ed2efdc0cc078033cd33c90e19d39da92b14e
Dean Pektas <deanpektas12345@gmail.com>
2023-11-09 15:32:01 -0500

    Fixed carousel in home page from pushing past width of page

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -5,27 +5,25 @@ import './../styles/home-carousel.css'
 
 export default function HomeCarousel() {
     return(
-        <Row>
-            <Carousel>
-                <Carousel.Item className="carousel-image bg-img-1">
-                    <Carousel.Caption>
-                        <h3>LEARN FROM THE BEST IN THE INDUSTRY</h3>
-                        <Button size="lg">Get Inspired</Button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item className="carousel-image bg-img-2">
-                    <Carousel.Caption>
-                        <h3>BRING YOUR KNOWLEDGE</h3>
-                        <Button size="lg">Volunteer</Button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item className="carousel-image bg-img-3">
-                    <Carousel.Caption>
-                        <h3>CONTRIBUTE TO STUDENTS' FUTURE</h3>
-                        <Button size="lg">Donate</Button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </Row>
+        <Carousel>
+            <Carousel.Item className="carousel-image bg-img-1">
+                <Carousel.Caption>
+                    <h3>LEARN FROM THE BEST IN THE INDUSTRY</h3>
+                    <Button size="lg">Get Inspired</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-image bg-img-2">
+                <Carousel.Caption>
+                    <h3>BRING YOUR KNOWLEDGE</h3>
+                    <Button size="lg">Volunteer</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="carousel-image bg-img-3">
+                <Carousel.Caption>
+                    <h3>CONTRIBUTE TO STUDENTS' FUTURE</h3>
+                    <Button size="lg">Donate</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }

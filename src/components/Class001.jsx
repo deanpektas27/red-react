fcdaf7d277647e3b4d9b1b3ff695524c86dc9854
Dean Pektas <deanpektas12345@gmail.com>
2024-01-03 16:38:09 -0500

    Added student headshots to graduating class page

--- src/components/Class001.jsx
+++ src/components/Class001.jsx
@@ -15,19 +15,71 @@ import Yero from './../assets/class001/Yero.png';
 
 export default function Class001() {
     return (
-        <div className="main-headline pt-5">
+        <div className="main-headline pb-5">
             <div className="container class-title">
                 <h1 className="title">CLASS 001 - "THE ORIGINALS"</h1>
             </div>
-            <div className="student">
-                <img src={Abdul} alt="" class="headshot" />
+            <div className="row" id="row1">
+                <div className="col" id="student">
+                    <img className="headshot" src={Katellyn} alt="" />
+                    <h5 className="">Katellyn Ahuati</h5>
+                    <p>CUNY Baruch College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Edryan} alt="" />
+                    <h5 className="">Edryan Canario</h5>
+                    <p>CUNY Baruch College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Yero} alt="" />
+                    <h5 className="">Yero Diamanka</h5>
+                    <p>University of Chicago</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Natalia} alt="" />
+                    <h5 className="">Natalia Diaz Villatoro</h5>
+                    <p>Manhattan College</p>
+                </div>
             </div>
-            <div className="student">
-                <img src={Diogo} alt="" class="headshot" />
+            <div className="row" id="row2">
+                <div className="col" id="student">
+                    <img className="headshot" src={Gabriel} alt="" />
+                    <h5 className="">Gabriel Fernandez</h5>
+                    <p>CUNY Baruch College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Abdul} alt="" />
+                    <h5 className="">Abdul Ganiyuajani</h5>
+                    <p>CUNY City College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Juliana} alt="" />
+                    <h5 className="">Juliana Martinez</h5>
+                    <p>CUNY Hunter College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Jason} alt="" />
+                    <h5 className="">Jason Maza</h5>
+                    <p>CUNY Baruch College</p>
+                </div>
             </div>
-
-            <img src={Edryan} alt="" class="headshot" />
-            <img src={Gabriel} alt="" class="headshot" />
+            <div className="row" id="row3">
+                <div className="col" id="student">
+                    <img className="headshot" src={Diogo} alt="" />
+                    <h5 className="">Diogo Montalvo</h5>
+                    <p>New York University</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Nick} alt="" />
+                    <h5 className="">Nick Rodriguez</h5>
+                    <p>Swarthmore College</p>
+                </div>
+                <div className="col" id="student">
+                    <img className="headshot" src={Jazmin} alt="" />
+                    <h5 className="">Jazmin Solano</h5>
+                    <p>CUNY Baruch College</p>
+                </div>
+            </div>
 
         </div>
     )

6934a19c26512b1a32999f56d093c48f8e35381f
Dean Pektas <deanpektas12345@gmail.com>
2025-01-26 17:38:11 -0500

    updated content

--- src/components/Class001.jsx
+++ src/components/Class001.jsx
@@ -61,9 +61,9 @@ export default function Class001() {
                             </div>
                             <div className="pb-2">
                                 <div className="container">
-                                    <p id="modal-student-school">{student[3] + student[4]} + {student[5]}</p>
-                                    {/* <p id="modal-student-school">{student[4]}</p>
-                                    <p id="modal-student-school">{student[5]}</p> */}
+                                    <p id="modal-student-school">{student[3]}</p>
+                                    <p id="modal-student-school">{student[4]}</p>
+                                    <p id="modal-student-school">{student[5]}</p>
                                 </div>
                             </div>
                         </div>  
